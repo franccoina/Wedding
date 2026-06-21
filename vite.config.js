6698ea5b753b13da80eb2df4a835ba6ea10900ba
David Francisco Blandón Mena <franccoina@gmail.com>
2026-06-21 03:35:24 -0500

    feat: add automatic sitemap generation with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: "https://www.danielaymichael.site",
+    }),
   ],
 });
